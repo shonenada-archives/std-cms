7ca98231f2de37380943381f7f1993c4f6cb9afa
shonenada <shonenada@gmail.com>
2014-06-07 00:40:48 +0800

    remove MenuCraeteSub

--- stdcms/Controller/Admin/MenuCreate.php
+++ stdcms/Controller/Admin/MenuCreate.php
@@ -18,57 +18,60 @@ class MenuCreate extends AdminBase {
     }
 
     static public function post() {
-        $post = urldecode(self::$request->params('menus'));
-        $menu = json_decode($post, true);
+        $data = self::$request->post();
+
+        $parentMenu = MenuModel::find($data['parent']);
 
         $success = true;
         $info = '';
 
-        if (!is_numeric($menu['sort'])) {
+        if (!is_numeric($data['sort'])) {
             $success = false;
             $info = '排序只能是数字';
         }
 
-        if (empty($menu['title'])) {
+        if (empty($data['title'])) {
             $success = false;
             $info = '标题不能为空';
         }
 
-        if (!isset($menu['type'])) {
+        if (!isset($data['type'])) {
             $success = false;
             $info = '类型不能为空';
         }
 
-        if ($menu['type'] == 3 && empty($menu['outside_url'])) {
+        if ($data['type'] == 3 && empty($data['outside_url'])) {
             $success = false;
             $info = '外部链接不能为空';
         }
 
-        if (!isset($menu['open_style'])) {
+        if (!isset($data['open_style'])) {
             $success = false;
             $info = '打开方式不能为空';
         }
 
-        if (empty($menu['is_show'])) {
+        if (empty($data['is_show'])) {
             $success = false;
             $info = '显示不能为空';
         }
 
         if ($success) {
             $new_menu = new MenuModel();
-            $new_menu->populateFromArray($menu)->save();
+            $new_menu->populateFromArray($data);
+            $new_menu->parent = $parentMenu;
+            $new_menu->save();
 
             $content = new MenuContent();
             $content->target = $new_menu;
             $content->lang = \GlobalEnv::get('translation.default');
-            $content->title = $menu['title'];
+            $content->title = $data['title'];
             $content->save();
 
-            if (isset($menu['title_eng'])) {
+            if (isset($data['title_eng'])) {
                 $content_eng = new MenuContent();
                 $content_eng->target = $new_menu;
                 $content_eng->lang = \Model\Lang::getByCode('en');
-                $content_eng->title = $menu['title_eng'];
+                $content_eng->title = $data['title_eng'];
                 $content_eng->save();
             }
         }
