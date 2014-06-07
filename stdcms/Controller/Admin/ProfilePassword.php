c95a65683e7a699ded52c98145eb0bd483632711
shonenada <shonenada@gmail.com>
2014-06-07 16:32:07 +0800

    update

--- stdcms/Controller/Admin/ProfilePassword.php
+++ stdcms/Controller/Admin/ProfilePassword.php
@@ -15,21 +15,32 @@ class ProfilePassword extends AdminBase {
     static public function post () {
         $app = \GlobalEnv::get('app');
         $user = \GlobalEnv::get('user');
+        $info = '';
+        $success = true;
         $old = self::$request->post('oldpassword');
         if (!$user->checkPassword($old, $app->config('salt'))) {
-            $msg = '旧密码错误，请重新输入';
-        return self::render('admin/profile_password.html', get_defined_vars());
+            $info = '旧密码错误，请重新输入';
+            $success = false;
         }
         $new = self::$request->post('newpassword');
         $confirm = self::$request->post('confirmpassword');
-        if ($new != $confirm) {
-            $msg = '确认密码不匹配，请重新输入';
-            return self::render('admin/profile_password.html', get_defined_vars());
+        if (strlen($new) <= 0) {
+            $info = '请输入密码';
+            $success = false;
         }
-        $user->setPassword($confirm, $app->config('salt'));
-        $user->save();
-        $msg = '修改成功';
-        return self::render('admin/profile_password.html', get_defined_vars());
+        if ($new != $confirm) {
+            $info = '确认密码不匹配，请重新输入';
+            $success = false;
+        }
+        if ($success) {
+            $user->setPassword($confirm, $app->config('salt'));
+            $user->save();
+        }
+        return json_encode(array(
+            'success' => $success,
+            'info' => $info,
+            ''
+        ));
     }
 
 }
